f4a8fabde4b849da05c6c8743789dcb16ba05809
pandi1813 <andrea.peter1813@gmail.com>
2023-01-06 10:28:47 +0000

    created prompt to ask user for required password length and confirmations to check which character types they would like to use. Saved them in variables to store user input

--- pseudocode.js
+++ pseudocode.js
@@ -1,7 +1,10 @@
 // Prompt the user to enter how many characters they want the password to be
+    // store returned value in a variable - how?
 
 //use confirmations to ask which character types they want to use
     // concat arrays that return true
+
+    // look up how to use confirm
 
 // creating password
     //use the new concat array to select characters from
--- script.js
+++ script.js
@@ -88,10 +88,28 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
 
-}
+let requiredPasswordLength = prompt("How many characters would you like to use?");
+let addLowerCasedCharacters = confirm("Would you like to use lower cased characters?");
+let addUpperCasedCharacters = confirm("Would you like to use upper cased characters?");
+let addNumbers = confirm("Would you like to use numbers?");
+let addSpecialCharacters = confirm("Would you like to use special characters?");
+
+let listOfChosenCharacters = [];
+
+//create 1 long array
+
+// chosenCharacterTypes();
+// function chosenCharacterTypes(array1, array2, array3, array4) {
+//     if (addLowerCasedCharacters === true) {
+//         listOfChosenCharacters = listOfChosenCharacters.concat(lowerCasedCharacters)
+//         return listOfChosenCharacters
+//     }
+// }
+// Function to prompt user for password options
+// function getPasswordOptions() {
+  
+// }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
