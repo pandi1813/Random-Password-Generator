966c71673f1388b1c29da448f1065f70ae2923a7
pandi1813 <andrea.peter1813@gmail.com>
2023-01-06 07:27:05 +0000

    initial commit

---
+++ pseudocode.js
@@ -0,0 +1,12 @@
+// Prompt the user to enter how many characters they want the password to be
+
+//use confirmations to ask which character types they want to use
+    // concat arrays that return true
+
+// creating password
+    //use the new concat array to select characters from
+
+    //create random number to use as index numbers 
+
+    // for loop to select the required number of characters
+        //how many times to run it = the return value from prompt
