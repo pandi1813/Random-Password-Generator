23b91e0219f23a37d0c844201f512647ab34ab2a
pandi1813 <andrea.peter1813@gmail.com>
2023-01-08 21:39:20 +0000

    created README file and pasted the template

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,75 +103,75 @@ let generatedPassword = [];
 
 // function to prompt the get password requirements
 function getPasswordOptions() {
-  
+
   passwordLength = prompt("How many characters would you like to use?");
-  
-    // if statements to set password length limits
+
+  // if statements to set password length limits
   if (passwordLength < 10) {
-    return alert("Password too short, please select at least 10 characters");     
+    return alert("Password too short, please select at least 10 characters");
   };
-  
-  if (passwordLength  > 64) {
+
+  if (passwordLength > 64) {
     return alert("Password too long, please select maximum 64 characters");
   }
 
 
-   addLowerCasedCharacters = confirm("Would you like to use lower cased characters?");
-   addUpperCasedCharacters = confirm("Would you like to use upper cased characters?");
-   addNumbers = confirm("Would you like to use numbers?");
-   addSpecialCharacters = confirm("Would you like to use special characters?");
+  addLowerCasedCharacters = confirm("Would you like to use lower cased characters?");
+  addUpperCasedCharacters = confirm("Would you like to use upper cased characters?");
+  addNumbers = confirm("Would you like to use numbers?");
+  addSpecialCharacters = confirm("Would you like to use special characters?");
 
-   selectCharacterTypes();
-   generateRandomNumbers();
+  selectCharacterTypes();
+  generateRandomNumbers();
 }
 
 
 // function to create 1 long array of the required character types
 // selectCharacterTypes();
 function selectCharacterTypes() {
-  
-    if (addLowerCasedCharacters === true) {
-        selectedCharactersArray = selectedCharactersArray.concat(lowerCasedCharacters)
-    };
-    if (addUpperCasedCharacters === true) {
-        selectedCharactersArray = selectedCharactersArray.concat(upperCasedCharacters);
-    };
-    if (addNumbers === true) {
-        selectedCharactersArray = selectedCharactersArray.concat(numericCharacters)
-    };
-    if (addSpecialCharacters === true) {
-        selectedCharactersArray = selectedCharactersArray.concat(specialCharacters)
-    };
-  
+
+  if (addLowerCasedCharacters === true) {
+    selectedCharactersArray = selectedCharactersArray.concat(lowerCasedCharacters)
+  };
+  if (addUpperCasedCharacters === true) {
+    selectedCharactersArray = selectedCharactersArray.concat(upperCasedCharacters);
+  };
+  if (addNumbers === true) {
+    selectedCharactersArray = selectedCharactersArray.concat(numericCharacters)
+  };
+  if (addSpecialCharacters === true) {
+    selectedCharactersArray = selectedCharactersArray.concat(specialCharacters)
+  };
+
 }
 
 // function to generate an array of random number to use as index for the joined array
 function generateRandomNumbers() {
   for (let i = 0; i < passwordLength; i++) {
     randomNumber.push(Math.floor(Math.random() * selectedCharactersArray.length));
-    
+
   };
-  
+
 }
 // console.log(randomNumber)
 
 // select characters from the joined array and create a string as a random password
 
 function generatePassword() {
-console.log(selectedCharactersArray);
-    
-    for (let i = 0; i < passwordLength; i++) {
-      generatedPassword.push(selectedCharactersArray[randomNumber[i]]);
-      console.log(randomNumber[i]);
-    }
+  console.log(selectedCharactersArray);
 
-    console.log(generatedPassword)
-    let stringPassword = generatedPassword.join("");
-    console.log(stringPassword)
-    
-    return stringPassword   
+  for (let i = 0; i < passwordLength; i++) {
+    generatedPassword.push(selectedCharactersArray[randomNumber[i]]);
+    console.log(randomNumber[i]);
+  }
+
+  console.log(generatedPassword)
+  let stringPassword = generatedPassword.join("");
+  console.log(stringPassword)
+
+  return stringPassword
 }
- 
+
 
 
 
